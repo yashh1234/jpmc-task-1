167cc8d9680056921d33554c2e8affce8d978acf
yashkumar <yashkumar@gupta.com>
2022-10-08 09:19:22 +0530

    Made changes to client.py

--- client.py
+++ client.py
@@ -43,7 +43,7 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     if(price_b == 0):
-        return
+                    return
     return price_a/price_b
 
 
